2a32b55acf45371fb1191a16cdee26b1ceeed62a
Sergio Zharinov <sergio.zharinov@gmail.com>
2019-11-30 17:49:28 +0400

    Set renovate/ruby base image

--- build.sh
+++ build.sh
@@ -5,6 +5,6 @@ export LOOKUP_NAME=CocoaPods/CocoaPods
 export VERSION_SCHEME=ruby
 export START_VERSION=1.7.2
 export IMAGE=cocoapods
-export IGNORED_VERSIONS=1.8.0.beta.1,1.8.0.beta.2
+export IGNORED_VERSIONS=
 
 npx renovatebot/dockerbuilder
